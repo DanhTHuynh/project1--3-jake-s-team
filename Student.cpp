5fbed957030753683b71c74943de3844c0c16620
Danh Huynh <36426308+DanhTHuynh@users.noreply.github.com>
2018-02-20 16:39:15 -0800

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -12,21 +12,41 @@ Student::Student() {
 
 Student::Student(const string &cwid) {
 	// TO BE COMPLETED
+	this -> cwid = cwid;
+	coursecount = 0;
 
 }
 
 string Student::getCWID() {
 	// TO BE COMPLETED
+	return cwid;
 }
 
 void Student::addCourseGrade (const string &courseName, char grade) {
 	// TO BE COMPLETED
+	this -> courseName[courseCount] = courseName;
+	this -> grade[courseCount] = grade;
+	courseCount ++;	//counter; increase
 
 }
 
 double Student::getGPA() {
 	// TO BE COMPLETED
-
+	int cScore;
+	int i;
+	cScore = i;
+	for(i = 0, i < courseCount; i++){
+		if(grade[i] >= 'A' && grade[i] <= 'E'){
+			cScore += 4 - (grade[i] <= 'E');
+		}
+		else if(grade[i] >= 'a' && grade[i] <= 'e'){
+			cScore += 4 - (grade[i] - 'a');
+		}
+	}
+	if(courseCount == 0){
+		return 0.0;
+	}
+	return (double) cScore/courseCount;
 }
 
 // print transcript in this (sample) format:
@@ -37,6 +57,15 @@ double Student::getGPA() {
 // GPA = 2.6667
 void Student::printTranscript() {
 	// TO BE COMPLETED
+	int cScore;
+	int i;
+	cScore = i;
+	cout << "TRANSCRIPT for CWID = " << cwid << endl;
+	for(i = 0, i < courseCount; i++){
+		cout << coursename[i] << " " << grade[i] << endl;
+		cScore += 4 - (grade[i] - 'A');
+	}
+	cout << "GPA = " << getGPA() << endl;
 
 }
 
